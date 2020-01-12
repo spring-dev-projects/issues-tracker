1d7de1fc8524cb8a98d8a4a8f7ddcfe9a4f9a70a
Kemal Gulpinar <kemalgulpnar@gmail.com>
2020-01-12 03:24:28 -0500

    database schema file creation

---
+++ src/main/java/com/kemalgulpinar/boot/issuetr/issuetracker/events/GithubProject.java
@@ -0,0 +1,58 @@
+package com.kemalgulpinar.boot.issuetr.issuetracker.events;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class GithubProject implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private String orgName;
+
+    @Column(unique=true)
+    private String repoName;
+
+    public GithubProject(String orgName, String repoName){
+        this.orgName = orgName;
+        this.repoName = repoName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getOrgName() {
+        return orgName;
+    }
+
+    public void setOrgName(String orgName) {
+        this.orgName = orgName;
+    }
+
+    public String getRepoName() {
+        return repoName;
+    }
+
+    public void setRepoName(String repoName) {
+        this.repoName = repoName;
+    }
+
+    @Override
+    public String toString() {
+        return "GithubProject{" +
+                "id=" + id +
+                ", orgName='" + orgName + '\'' +
+                ", repoName='" + repoName + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/kemalgulpinar/boot/issuetr/issuetracker/events/GithubProjectRepository.java
@@ -0,0 +1,8 @@
+package com.kemalgulpinar.boot.issuetr.issuetracker.events;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface GithubProjectRepository extends PagingAndSortingRepository<GithubProject, Long> {
+
+    GithubProject findByRepoName(String repoName);
+}
